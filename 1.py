ce5d455907e49d3504e4fcd2bdea1c232542e3af
shivankar <shivankar.s@greyorange.com>
2025-04-21 14:07:00 +0530

    1 Leetcode

--- 1.py
+++ 1.py
@@ -1,10 +1,15 @@
 # https://leetcode.com/problems/two-sum/?envType=problem-list-v2&envId=9colawc6
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        return []
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    dict = {}
+    for i in range(0, len(nums)):
+        if nums[i] in dict:
+            return [dict[nums[i]], i]
+        else:
+            dict[target - nums[i]] = i
+    return []
